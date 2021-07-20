47bcb1e2d4e02fd1f55ea0333d4b48a1dc3698dd
OlgaKireenko <kireenko86.osil@mail.ru>
2021-07-20 23:37:38 +0300

    code modified2

--- src/test/java/ru/RushAnalytics/TestRegistration.java
+++ src/test/java/ru/RushAnalytics/TestRegistration.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
@@ -27,6 +28,12 @@ public class TestRegistration {
 
     @BeforeEach
     void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+
+        
         driver = new ChromeDriver();
     }
 

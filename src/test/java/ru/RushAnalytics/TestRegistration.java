009d3d0f59c8db5f4c0ae9970313f1477bc9b200
OlgaKireenko <kireenko86.osil@mail.ru>
2021-07-21 09:51:11 +0300

    code modified

--- src/test/java/ru/RushAnalytics/TestRegistration.java
+++ src/test/java/ru/RushAnalytics/TestRegistration.java
@@ -28,42 +28,27 @@ public class TestRegistration {
 
     @BeforeEach
     void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
 
-
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
     }
-
     @AfterEach
     void tearDown() {
-
         driver.quit();
         driver = null;
     }
     @Test
     public void registrationTest1() {
         driver.get("https://www.rush-analytics.ru/");
-        driver.findElement(By.className("btn-ra-menu-register")).click();
-
-        driver.findElement(By.cssSelector("#edit-name")).sendKeys("kireenko86.osil@mail.ru");
-        driver.findElement(By.cssSelector("#edit-field-phone-und-0-value")).sendKeys("+79270000000");
-        driver.findElement(By.cssSelector("#edit-field-name-und-0-value")).sendKeys("Olga");
+       driver.findElement(By.className("btn-ra-menu-register")).click();
+      driver.findElement(By.cssSelector("#edit-name")).sendKeys("kireenko86.osil@mail.ru");
+      driver.findElement(By.cssSelector("#edit-field-phone-und-0-value")).sendKeys("+79270000000");
+       driver.findElement(By.cssSelector("#edit-field-name-und-0-value")).sendKeys("Olga");
         driver.findElement(By.cssSelector("#edit-field-terms-und")).click();
-        driver.findElement(By.cssSelector("#edit-submit")).click();
-        String text = driver.findElement(By.cssSelector("#messages")).getText();
-        assertEquals("Error message\n" +
-                "Этот е-мейл адрес уже зарегистрирован.Забыли пароль?", text.trim());
-
-
-
-        //Thread.sleep(5000);
-    }
-
-
-
+       driver.findElement(By.cssSelector("#edit-submit")).click();
+       String text = driver.findElement(By.cssSelector("#messages")).getText();
+      assertEquals("Error message\n" +
+               "Этот е-мейл адрес уже зарегистрирован.Забыли пароль?", text.trim());
+            }
 }
 
 
